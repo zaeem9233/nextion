039f221df3b7fc0841308734ed80f5d49f8d2b1e
zaeem9233 <zaeemali91@gmail.com>
2024-01-12 15:57:08 +0500

    Logo added in index or main page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,12 +7,17 @@
   <div class="container mt-5">
       <div class="card text-center mx-auto" style="max-width: 400px;">
           <div class="card-body">
-              <h2 class="card-title">Access to Dashboard</h2>
-              <div class="btn-group-vertical mt-4">
-                  <a href="/login" class="btn btn-primary">Login</a>
+            
+            <div class="d-flex justify-content-center mb-4">
+                <img src="/images/logo.png" alt="Logo" class="img-fluid" style="max-height: 50px;">
+            </div>
 
-                  <a href="/register" class="btn btn-success">Register</a>
-              </div>
+            <h2 class="card-title">Access to Dashboard</h2>
+            <div class="mt-4">
+                <a href="/login" class="btn btn-primary">Login</a>
+
+                <a href="/register" class="btn btn-success">Register</a>
+            </div>
           </div>
       </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index'])->name('home');
+Route::get('/login', [MainController::class, 'index'])->name('login');
+Route::get('/register', [MainController::class, 'index'])->name('register');
